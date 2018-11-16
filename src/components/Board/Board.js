978eb77090fe82b932ccb52f184ca09c439e3937
metamn <bartus.csongor@gmail.com>
2018-11-16 21:20:54 +0200

    Adding a restart button

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -4,21 +4,46 @@ import styled from 'styled-components';
 import Square from './../Square';
 
 /**
- * Styles
- *
+ * The container style.
  */
-const Container = styled.section``;
-
-const ContainerTitle = styled.h3`
-	display: ${props => props.hidden ? 'none' : 'flex'};
+const Container = styled.section`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	alig-items: center;
 `;
 
+/**
+ * The container title.
+ */
+const ContainerTitle = styled.h3`
+	display: ${props => props.hidden ? 'none' : 'flex'};
+	align-self: center;
+`;
+
+/**
+ * The squares container style.
+ */
 const SquaresContainer = styled.div`
 	border-right: 1px solid;
 	border-bottom: 1px solid;
 `;
 
+/**
+ * The row style in the squares container.
+ */
 const SquaresRow = styled.div``;
+
+/**
+ * The restart button style.
+ */
+const Button = styled.button`
+	margin: 1.25em 0;
+	padding: .625em 0;
+	background: white;
+	border: 1px solid;
+`;
+
 
 /**
  * Imitates the for loop inside JSX
@@ -50,6 +75,11 @@ export default class Board extends React.Component {
 		};
 	}
 
+	/**
+	 * Handles the click on a square.
+	 *
+	 * @param  {integer} i The ID of the square
+	 */
 	handleClick(i) {
 		// Do nothing if already clicked.
 		if (this.state.squares[i] != null) return;
@@ -58,7 +88,7 @@ export default class Board extends React.Component {
 		const squares = this.state.squares.slice();
 
 		// Mark the cell.
-		squares[i] = (this.state.turn == 'X') ? 'O' : 'X';
+		squares[i] = this.state.turn;
 
 		// Set state
 		this.setState({
@@ -67,6 +97,12 @@ export default class Board extends React.Component {
 		});
 	}
 
+	/**
+	 * Renders a square.
+	 *
+	 * @param  {integer} i The ID of a square.
+	 * @return {Square}    A square element.
+	 */
 	renderSquare(i) {
 		return (
 			<Square
@@ -76,10 +112,33 @@ export default class Board extends React.Component {
 		);
 	}
 
+	/**
+	 * Displays the component title.
+	 *
+	 * @return {string} The title
+	 */
+	displayTitle() {
+		if (this.state.squares.filter(item => item == null).length == 0) {
+			return 'No more moves';
+		}
+
+		return 'Next turn: ' + this.state.turn;
+	}
+
+	/**
+	 * Restarts the game
+	 */
+	restart() {
+		this.setState({
+			squares: Array(9).fill(null),
+			turn: 'X',
+		});
+	}
+
 	render() {
 		return (
 			<Container>
-				<ContainerTitle hidden>The board</ContainerTitle>
+				<ContainerTitle>{this.displayTitle()}</ContainerTitle>
 				<SquaresContainer>
 					<Repeat numberOfTimes={3} startAt={0}>
 						{(i) =>
@@ -91,6 +150,7 @@ export default class Board extends React.Component {
 						}
 					</Repeat>
 				</SquaresContainer>
+				<Button onClick={() => this.restart()}>Restart</Button>
 			</Container>
 		)
 	};
