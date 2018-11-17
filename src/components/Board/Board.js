21898d5aea89f8beaf7f5bed5d71b07ac8ba69b0
metamn <bartus.csongor@gmail.com>
2018-11-17 14:58:20 +0200

    Lifting state from Board to Game

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -46,94 +46,9 @@ const Button = styled.button`
 `;
 
 /**
- * Defines which lines are a winning combination.
- */
-const winningLines = [
-	[0, 1, 2],
-	[3, 4, 5],
-	[6, 7, 8],
-	[0, 3, 6],
-	[1, 4, 7],
-	[2, 5, 8],
-	[0, 4, 8],
-	[2, 4, 6],
-];
-
-/**
- * Checks if there is a winner.
- *
- * @param  {array}  squares The squares.
- * @return {object}         The ID of the winner, and the winning line
- */
-function hasWinner(squares) {
-	for (let i = 0; i < winningLines.length; i++) {
-		const [a, b, c] = winningLines[i];
-
-		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-	  		return {
-				winnerID: squares[a],
-				winningSquares: winningLines[i],
-			};
-		}
-	}
-
-	return '';
-}
-
-
-/**
  * The main class
  */
 export default class Board extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			squares: Array(9).fill(null),
-			winningSquares: Array(3).fill(null),
-			winnerID: null,
-			turn: 'X',
-		};
-	}
-
-	/**
-	 * Handles the click on a square.
-	 *
-	 * @param  {integer} i The ID of the square
-	 */
-	handleClick(i) {
-		// Do nothing if there is a winner
-		if (hasWinner(this.state.squares)) return;
-
-		// Do nothing if square is already clicked.
-		if (this.state.squares[i] != null) return;
-
-		// Handle the current click
-		//
-		// Implementing immutability
-		const squares = this.state.squares.slice();
-		let winningSquares = this.state.winningSquares.slice();
-		let winnerID = this.state.winnerID;
-
-		// Mark the cell.
-		squares[i] = this.state.turn;
-
-		// Check if there is a winner after this current move
-		let winner = hasWinner(squares);
-		if (winner) {
-			winningSquares = winner.winningSquares;
-			winnerID = winner.winnerID;
-		}
-
-		// Set state
-		this.setState({
-			squares: squares,
-			winningSquares: winningSquares,
-			winnerID: winnerID,
-			turn: (this.state.turn == 'X') ? 'O' : 'X',
-		});
-	}
-
 	/**
 	 * Renders a square.
 	 *
@@ -141,11 +56,14 @@ export default class Board extends React.Component {
 	 * @return {Square}    A square element.
 	 */
 	renderSquare(i) {
+		const squares = this.props.current.squares;
+		const winningSquares = this.props.winningSquares;
+
 		return (
 			<Square
-				value={this.state.squares[i]}
-				onClick={() => this.handleClick(i)}
-				status={(this.state.winningSquares.includes(i))}
+				value={squares[i]}
+				onClick={() => this.props.handleClick(i)}
+				status={(winningSquares.includes(i))}
 			/>
 		);
 	}
@@ -156,27 +74,19 @@ export default class Board extends React.Component {
 	 * @return {string} The title
 	 */
 	displayTitle() {
-		if (this.state.winnerID) {
-			return this.state.winnerID + ' wins!';
+		const winnerID = this.props.winnerID;
+		const squares = this.props.current.squares;
+		const turn = this.props.turn;
+
+		if (winnerID) {
+			return winnerID + ' wins!';
 		}
 
-		if (this.state.squares.filter(item => item == null).length == 0) {
+		if (squares.filter(item => item == null).length == 0) {
 			return 'No more moves';
 		}
 
-		return 'Next turn: ' + this.state.turn;
-	}
-
-	/**
-	 * Restarts the game.
-	 */
-	restart() {
-		this.setState({
-			squares: Array(9).fill(null),
-			winningSquares: Array(3).fill(null),
-			winnerID: null,
-			turn: 'X',
-		});
+		return 'Next turn: ' + turn;
 	}
 
 	render() {
@@ -194,7 +104,7 @@ export default class Board extends React.Component {
 						}
 					</Repeat>
 				</SquaresContainer>
-				<Button onClick={() => this.restart()}>Restart</Button>
+				<Button>Restart</Button>
 			</Container>
 		)
 	};
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -22,6 +22,43 @@ const ContainerTitle = styled.h1``;
 
 
 /**
+ * Defines which lines are a winning combination.
+ */
+const winningLines = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6],
+];
+
+
+/**
+ * Checks if there is a winner.
+ *
+ * @param  {array}  squares The squares.
+ * @return {object}         The ID of the winner, and the winning line
+ */
+function hasWinner(squares) {
+	for (let i = 0; i < winningLines.length; i++) {
+		const [a, b, c] = winningLines[i];
+
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+	  		return {
+				winnerID: squares[a],
+				winningSquares: winningLines[i],
+			};
+		}
+	}
+
+	return '';
+}
+
+
+/**
  * Displays the game.
  */
 export default class Game extends React.Component {
@@ -29,16 +66,71 @@ export default class Game extends React.Component {
 		super(props);
 
 		this.state = {
-			history: Array(),
+			history: [{
+				squares: Array(9).fill(null),
+			}],
+			winningSquares: Array(3).fill(null),
+			winnerID: null,
+			turn: 'X',
 		}
 	}
-	
+
+	/**
+	 * Handles the click on a square.
+	 *
+	 * @param  {integer} i The ID of the square
+	 */
+	handleClick(i) {
+		const history = this.state.history;
+		const current = history[history.length - 1];
+		const squares = current.squares.slice();
+		const turn = this.state.turn;
+		let winningSquares = this.state.winningSquares.slice();
+		let winnerID = this.state.winnerID;
+
+		// Do nothing if there is a winner
+		if (hasWinner(squares)) return;
+
+		// Do nothing if square is already clicked.
+		if (squares[i] != null) return;
+
+		// Mark the cell.
+		squares[i] = turn;
+
+		// Check if there is a winner after this current move
+		let winner = hasWinner(squares);
+		if (winner) {
+			winningSquares = winner.winningSquares;
+			winnerID = winner.winnerID;
+		}
+
+		// Set state
+		this.setState({
+			squares: squares,
+			winningSquares: winningSquares,
+			winnerID: winnerID,
+			turn: (turn == 'X') ? 'O' : 'X',
+		});
+	}
+
 	render() {
+		const history = this.state.history;
+		const current = history[history.length - 1];
+		const winningSquares = this.state.winningSquares;
+		const winnerID = this.state.winnerID;
+		const turn = this.state.turn;
+
 		return (
 			<Container>
 				<ContainerTitle>Tic Tac Toe</ContainerTitle>
-				<Board/>
-				<History history={this.state.history}/>
+				<Board
+					current={current}
+					winningSquares={winningSquares}
+					winnerID={winnerID}
+					turn={turn}
+					handleClick={((i) => this.handleClick(i))}
+				/>
+				<History history={history}/>
 			</Container>
 		);
 	}
